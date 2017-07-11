85c3f295e5e91b53ad0520c794b288d9dce6d8c6
hyliu <liuhaiyang1992@hotmail.com>
2017-07-11 16:01:36 +0800

    init

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'electron-1st',
+  };
+</script>
+
+<style>
+  /* CSS */
+</style>
