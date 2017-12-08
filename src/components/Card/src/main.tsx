a70889c61515aa8015a960d4dbfa0f0cd2645381
hyliu <liuhaiyang1992@hotmail.com>
2017-12-08 18:38:19 +0800

    some improve

--- src/components/Card/src/main.tsx
+++ src/components/Card/src/main.tsx
@@ -1,34 +1,53 @@
 import { Vue, Component, Prop } from "vue-property-decorator"
-@Component
-  export default class Card extends Vue {
-   @Prop() header: string
-   @Prop() bodyStyle: object
 
-    render (h) {
-      console.log(this.header)
-      let cardHeader = () => {
-        if (this.$slots.header || this.header) {
-          return (
-            <div
-              class="card__header">
-                {this.$slots.header || this.header}
-            </div>
-          )
-        }
-      }
-      console.log(cardHeader())
-      return (
+const card = {
+  padding: '20px',
+  borderRadius: '4px',
+  overflow: 'hidden',
+  boxShadow: '0 2px 12px 0 rgba(0, 0, 0, 0.1)',
+  color: '#2d2f33',
+  backgroundColor: '#fff',
+  border: '1px solid #e6ebf5',
+}
+const card_header = {
+  borderBottom: '1px solid #e6ebf5',
+  boxSizing: 'border-box'
+}
+
+@Component
+export default class Card extends Vue {
+  @Prop() header: string
+  @Prop() bodyStyle: object
+
+  render (h) {
+    return (
+      <div
+          style={card}>
+        {CardHeader(this.$slots.header,
+                    this.header,
+                    card_header,
+                    h)}
         <div
-           class="card">
-          {cardHeader()}
-          <div
-            style={this.bodyStyle}
-            class="card__body">
-            {this.$slots.default}
-          </div>
+          style={this.bodyStyle}>
+          {this.$slots.default}
         </div>
-      )
-    }
+      </div>
+    )
   }
-  
- 
+}
+
+function CardHeader (
+  header,
+  slotHeader,
+  card_header,
+  h
+) {
+  if (slotHeader || header) {
+    return (
+      <div
+        style={card_header}>
+        {slotHeader || header}
+      </div>
+    )
+  }
+}
